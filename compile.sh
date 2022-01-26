cc4088da8c370714ac080e04db4735a4c6f51fbd
simewu <swuthier@uccs.edu>
2022-01-26 16:09:04 -0700

    Working implementation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-make geth
+echo "If this errors, type \"go version\" and make sure it's up-to-date"
+echo "If it is not up-to-date, type \"which go\" and \"rm -rf\" that path"
+echo "Then re-install, and append \"export PATH=\$PATH:/usr/local/go/bin\" to \"\$HOME/.profile\""
+
+
+make all
