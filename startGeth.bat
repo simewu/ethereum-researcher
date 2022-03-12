26c5f8fadcf63b3afed02d718feccb94aff6f9ed
Simeon Wuthier <simeonwuthier@gmail.com>
2022-03-11 17:55:51 -0700

    Create startGeth.bat

---
+++ startGeth.bat
@@ -0,0 +1,2 @@
+start /b "" "C:\Program Files\Geth\geth" -http --http.port 1210 --ropsten console
+::start /b "" "C:\Program Files\Geth\geth" --syncmode "light" -http --http.port 1210 --ropsten console
